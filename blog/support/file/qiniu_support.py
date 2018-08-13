b12bc07166bf5043fe68d86e44863e28cde6448a
pengfei.you <pengfei.you@nio.com>
2018-08-13 14:26:03 +0800

    新增七牛文件

--- blog/support/file/qiniu_support.py
+++ blog/support/file/qiniu_support.py
@@ -1,20 +1,25 @@
 import qiniu.config
 from qiniu import Auth, etag, put_file
+from configparser import ConfigParser
+
+cf = ConfigParser()
+cf.read("/Users/pengfei.you/conf/config.ini")
 
 # 需要填写你的 Access Key 和 Secret Key
-access_key = 'LYgEIxfrKt7h6tf2ero1VrtwxxqUr1qmIuqhiV2n'
-secret_key = 'PTihHOOPlnNDgoi25fo4x7JvSZ8lX2zAw3cqXuUX'
-# 构建鉴权对象
-q = Auth(access_key, secret_key)
-# 要上传的空间
-bucket_name = 'image'
-# 上传到七牛后保存的文件名
-key = 'my-python-logo.jpeg'
-# 生成上传 Token，可以指定过期时间等
-token = q.upload_token(bucket_name, key, 3600)
-# 要上传文件的本地路径
-localfile = '/Users/pengfei.you/Downloads/WechatIMG14560.jpeg'
-ret, info = put_file(token, key, localfile)
-print(info)
-assert ret['key'] == key
-assert ret['hash'] == etag(localfile)
+access_key = cf.get("qiniu", "access_key")
+secret_key = cf.get("qiniu", "secret_key")
+
+
+def get_token(bucket_name, key, timeout):
+    '''
+    获取上传的token
+    :param bucket_name: 上传未知
+    :param key:  上传后的名称
+    :param timeout: token 超时时间
+    :return: 生成的token 给前端用
+    '''
+    # 构建鉴权对象
+    q = Auth(access_key, secret_key)
+    # 生成上传 Token，可以指定过期时间等
+    token = q.upload_token(bucket_name, key, timeout)
+    return token
